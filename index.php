ca63f5b2cd14c20a17318e8695249d0d125720e2
huyhhps23321 <98297761+huyhhps23321@users.noreply.github.com>
2023-12-20 21:16:05 +0700

    web test

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Viper shop</title>
+    <link rel="shortcut icon" href="/images/pngwing.com.png" type="image/x-icon">
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<header>
+    <h1 class="logo">Viper Shop</h1>
+    <ul>
+        <li><a href="/index.html">Home</a></li>
+        <li><a href="/pages/products.html">Products</a></li>
+        <li><a href="/pages/cart.html">Cart</a></li>
+        <li><a href="/pages/login.html">Login/Sign up</a></li>
+    </ul>
+</header>
+
+<main>
+    <div class="description">
+        <div class="center">
+            <h2 style="text-align: center; font-weight: bold; margin-top: 30px;">
+                Come meet your new best friend.
+            </h2>
+        
+            <p style="text-align: center; font-size: 13pt; margin-top: 20px;">
+                Welcome to our amazing world of exotic-pets! 
+                Here, on our website, we are dedicated to providing 
+                you with the exotic pets you need.
+            </p>
+        </div>
+        <button>Discover</button>
+    </div>
+    <div class="icon">
+        <img src="/images/pngwing.com.png" alt="">
+    </div>
+</main>
+
+<body>
+    
+    <section>
+        <h3>Bestseller</h3>
+        <div class="product">
+
+            <div class="pro1">
+                <img src="/images/beard dragon.jpg" alt="">
+                <h5>Beard dragon</h5>
+                <button>
+                    <a href="/pages/ctsp.html" style="text-decoration: none; color: #ECF3F6;">
+                        View
+                    </a>
+                </button>
+            </div>
+
+            <div class="pro1">
+                <img src="/images/corn snake.jpg" alt="">
+                <h5>Corn snake</h5>
+                <button>
+                    <a href="/pages/ctsp.html" style="text-decoration: none; color: #ECF3F6;">
+                        View
+                    </a>
+                </button>
+            </div>
+
+            <div class="pro1">
+                <img src="/images/pacman frog.jpg" alt="">
+                <h5>Pacman frog</h5>
+                <button>
+                    <a href="/pages/ctsp.html" style="text-decoration: none; color: #ECF3F6;">
+                        View
+                    </a>
+                </button>
+            </div>
+        </div>
+    </section>
+
+    <div class="delivery">
+        <div class="icon-delivery">
+            <p>Delivery</p>
+            <img src="/images/viper-shop-logo-removebg-preview.png" alt="">
+            <h6>In addition to offering a wide variety of tempting exotic-pets, 
+                we also provide the convenience and convenience of direct delivery 
+                services to your home.
+            </h6>
+        </div>
+    </div>
+</body>
+<footer>
+    <h4>terms of use and privacy</h4>
+</footer>
+</html>
